493a9161576c9a256a7101bd1bcfeb900d9aa85b
Michael Venezia <mvenezia@gmail.com>
2018-05-02 15:47:11 -0400

    Updating how swagger is configured and the local swagger file

---
+++ build/generators/swagger-dist-adjustment.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+THIS_DIRECTORY=$(dirname "${BASH_SOURCE}")
+PROJECT_DIRECTORY=${THIS_DIRECTORY}/../..
+
+echo
+echo "Adjusting swagger to default to our API's swagger file"
+sed -i "" 's/http:\/\/petstore.swagger.io\/v2\/swagger.json/..\/swagger\/api.swagger.json/' ${PROJECT_DIRECTORY}/third_party/swagger-ui/index.html
