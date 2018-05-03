a5444228124907bd9c49e511341cd36fdc391e8f
Michael Venezia <mvenezia@gmail.com>
2018-05-03 14:59:12 -0400

    Adding rudimentary example of creating KrakenCluster CR

--- internal/cluster-manager-api/service.go
+++ internal/cluster-manager-api/service.go
@@ -12,6 +12,8 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"github.com/juju/loggo"
 	"github.com/samsung-cnct/cluster-manager-api/pkg/util"
+
+	ccapi "github.com/samsung-cnct/cluster-controller/pkg/apis/clustercontroller/v1alpha1"
 )
 
 var (
@@ -43,6 +45,61 @@ func (s *Server) GetPodCount(ctx context.Context, in *pb.GetPodCountMsg) (*pb.Ge
 	if err != nil {
 		logger.Errorf("Cannot establish a client connection to kubernetes: %v", err)
 		return nil, err
+	}
+
+	dummy := &ccapi.KrakenCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "my-test-cluster",
+		},
+		Spec: ccapi.KrakenClusterSpec{
+			CustomerID: "myCustomerID",
+			CloudProvider: ccapi.CloudProviderInfo{
+				Name: "aws",
+				Credentials: ccapi.CloudProviderCredentials{
+					Username: "myuser",
+					Password: "fakepassword1",
+				},
+			},
+			Provisioner: ccapi.ProvisionerInfo{
+				Name: "juju",
+			},
+			Cluster: ccapi.ClusterInfo{
+				ClusterName: "my-test-cluster",
+				NodePools: []ccapi.NodeProperties{
+					{
+						Name: "worker",
+						PublicIPs: false,
+						Size: 1,
+						MachineType: "m4.xlarge",
+						Os: "ubuntu:16:04",
+					},
+					{
+						Name: "master",
+						PublicIPs: false,
+						Size: 1,
+						MachineType: "m4.xlarge",
+						Os: "ubuntu:16:04",
+					},
+					{
+						Name: "etcd",
+						PublicIPs: false,
+						Size: 3,
+						MachineType: "m3.medium",
+						Os: "ubuntu:16:04",
+					},
+				},
+				Fabric: ccapi.FabricInfo{
+					Name: "canal",
+				},
+			},
+		},
+	}
+
+	_, err = clusterControllerClient.KrakenCRCli.SamsungV1alpha1().KrakenClusters("default").Create(dummy)
+	if err != nil && !k8sutil.IsResourceAlreadyExistsError(err) {
+		logger.Infof("KrakenCluster -->%s<-- Cannot be created, error was %v", dummy.ObjectMeta.Name, err)
+	} else if k8sutil.IsResourceAlreadyExistsError(err) {
+		logger.Infof("KrakenCluster -->%s<-- Already exists, cannot recreate", dummy.ObjectMeta.Name)
 	}
 
 	logger.Infof("Was asked to get pods on -->%s<-- namespace, answer was -->%d<--", in.Namespace, int32(len(pods.Items)))
