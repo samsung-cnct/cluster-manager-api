3cebc5cf989ac36053b21efc218e39bbac788e4c
Michael Venezia <mvenezia@gmail.com>
2018-06-06 19:38:28 -0400

    Adjusting script for dist-adjustment to work with both mac/bsd sed and gnu sed

--- build/generators/swagger-dist-adjustment.sh
+++ build/generators/swagger-dist-adjustment.sh
@@ -9,4 +9,6 @@ PROJECT_DIRECTORY=${THIS_DIRECTORY}/../..
 
 echo
 echo "Adjusting swagger to default to our API's swagger file"
-sed -i "" 's/http:\/\/petstore.swagger.io\/v2\/swagger.json/..\/swagger\/api.swagger.json/' ${PROJECT_DIRECTORY}/third_party/swagger-ui/index.html
+echo "OK if you see \"sed: unsupported command u\" and this is running on linux/gnu-sed"
+(sed -i "dummy" 's/http:\/\/petstore.swagger.io\/v2\/swagger.json/..\/swagger\/api.swagger.json/' ${PROJECT_DIRECTORY}/third_party/swagger-ui/index.html) || (sed -idummy 's/http:\/\/petstore.swagger.io\/v2\/swagger.json/..\/swagger\/api.swagger.json/' ${PROJECT_DIRECTORY}/third_party/swagger-ui/index.html )
+rm ${PROJECT_DIRECTORY}/third_party/swagger-ui/index.htmldummy
